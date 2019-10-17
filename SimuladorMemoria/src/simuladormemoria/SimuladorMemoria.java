84afc3b40d3212e76732a54abf753cec25d7d974
Alejandra Gonzalez Carrillo <alegonza812@gmail.com>
2019-10-17 13:17:02 -0600

    interfaz inicial

---
+++ SimuladorMemoria/src/simuladormemoria/SimuladorMemoria.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladormemoria;
+
+import GUI.Central;
+
+/**
+ *
+ * @author Ale
+ */
+public class SimuladorMemoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Central c = new Central();
+        c.show();
+        
+    }
+    
+}
